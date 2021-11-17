2efab8deb06280b7fcc10f7aacf50f2bc0afef6b
Tomasz Posiadala <tomasz.posiadala@jaywing.com>
2021-11-17 20:15:57 +0000

    services changes

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Agency v7.0.6 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+* Start Bootstrap - Agency v7.0.6 (undefined)
+* Copyright 2013-2021 undefined
+* Licensed under undefined (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
 */
 //
 // Scripts
