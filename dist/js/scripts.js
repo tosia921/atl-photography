0e8b16e2f3298a5da99615593a3b4daea78afa1a
Tomasz Posiadala <tomaszposiadala@MacBook-003161.local>
2023-02-21 10:00:29 +0000

    closing website temporarly

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Agency v7.0.6 (undefined)
-* Copyright 2013-2022 undefined
+* Copyright 2013-2023 undefined
 * Licensed under undefined (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
 */
 //
